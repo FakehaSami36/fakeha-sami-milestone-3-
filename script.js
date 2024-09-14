cb20ba1e501fd9e4e0cd2b2b0014efb64609be12
FakehaSami36 <165374104+FakehaSami36@users.noreply.github.com>
2024-09-14 12:13:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+var _a;
+// listing element
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    //type assertion
+    const nameElement = document.getElementById('name');
+    const emailElement = document.getElementById('email');
+    const phoneElement = document.getElementById('phone');
+    const educationElement = document.getElementById('education');
+    const experienceElement = document.getElementById('experience');
+    const skillsElement = document.getElementById('skills');
+    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        const name = nameElement.value;
+        const email = emailElement.value;
+        const phone = phoneElement.value;
+        const education = educationElement.value;
+        const experience = experienceElement.value;
+        const skills = skillsElement.value;
+        //Create Resume Output
+        const resumeOutput = `
+<h2>Resume</h2>
+<p><strong>Name:</strong> ${name} </p>
+<p><strong>Email:</strong> ${email} </p>
+<p><strong>Phone Number:</strong> ${phone} </p>
+
+<h3>Education</h3>
+<p>${education}</p>
+
+<h3>Experience</h3>
+<p>${experience}</p>
+
+<h3>Skills</h3>
+<p>${skills}</p>
+
+`;
+        const resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        }
+        else {
+            console.error('the resume output elements are missing');
+        }
+    }
+    else {
+        console.error('one or more output elements are missing');
+    }
+});
